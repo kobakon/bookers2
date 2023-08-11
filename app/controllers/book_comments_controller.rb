d1be79be0a83d817db9a47213d0b10a64df622f8
kobakon <138418858+kobakon@users.noreply.github.com>
2023-08-11 09:56:22 +0000

    addいいね、コメントの非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id]) # パラメータから本を取得
-    comment = current_user.book_comments.new(book_comment_params) # ログイン中のユーザーに紐づいた新しいコメントを作成
-    comment.book_id = book.id # コメントに本のIDを設定
-    comment.save # データベースに保存
-    redirect_to request.referer
+    @book = Book.find(params[:book_id]) # パラメータから本を取得
+    @comment = current_user.book_comments.new(book_comment_params) # ログイン中のユーザーに紐づいた新しいコメントを作成
+    @comment.book_id = @book.id # コメントに本のIDを設定
+    @comment.save # データベースに保存
+    
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = @book.book_comments.find(params[:id])
+    @comment.destroy
+    
   end
 
   private
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,11 +3,11 @@ class SearchesController < ApplicationController
   
   def search
     @range = params[:range]
-  
     if @range == "User"
-      @user = User.looks(params[:search], params[:word])
+      @users = User.looks(params[:search],params[:word])
+      
     else
-      @book = Book.looks(params[:search], params[:word])
+      @books = Book.looks(params[:search], params[:word])
     end
   end
 end
